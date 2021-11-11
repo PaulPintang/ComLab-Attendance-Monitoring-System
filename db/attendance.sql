a8c761665610972f23b0d68837cb6b37d0e59391
puldyastin <puldyastin@gmail.com>
2021-11-11 15:31:11 +0800

    export db

--- db/attendance.sql
+++ db/attendance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 02, 2021 at 04:03 AM
+-- Generation Time: Nov 11, 2021 at 08:30 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -24,6 +24,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `course` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `course`) VALUES
+(1, 'Bachelor of Science in Information System'),
+(2, 'Bachelor of Science in Information Technology'),
+(3, 'Bachelor of Science in IT-Animation'),
+(4, 'Bachelor of Science in Computer Engineering'),
+(5, 'Bachelor of Science in Electronics Communication Engineering');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profaccounts`
+--
+
+CREATE TABLE `profaccounts` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `passcode` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `profaccounts`
+--
+
+INSERT INTO `profaccounts` (`id`, `name`, `username`, `passcode`) VALUES
+(26, 'dsadsadds', 'sadasd', 'asdasd'),
+(27, 'sa', 'dasd', 'asda');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `professor`
 --
 
@@ -36,6 +79,13 @@ CREATE TABLE `professor` (
   `timeIn` varchar(255) NOT NULL,
   `timeOut` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `professor`
+--
+
+INSERT INTO `professor` (`id`, `profName`, `profSubject`, `classCourseY`, `totalStud`, `timeIn`, `timeOut`) VALUES
+(1, 'asdas', 'dasd', 'asd', 21, '02:39 PM', '03:13');
 
 -- --------------------------------------------------------
 
@@ -57,7 +107,10 @@ INSERT INTO `studaccounts` (`id`, `studentName`, `studentId`) VALUES
 (1, 'Paul Justine Pintang', '2018-PC-100757'),
 (2, 'Jessie Santiano', '2018-PC-100345'),
 (3, 'Joner Rangasajo', '2018-PC-123'),
-(4, 'Mark Bryan Real', '2018-PC-100234');
+(4, 'Mark Bryan Real', '2018-PC-100234'),
+(5, 'dsadsaddsa', 'dsadas'),
+(6, 'dadad', 'dasd'),
+(7, 'dsadas', 'dasd');
 
 -- --------------------------------------------------------
 
@@ -73,9 +126,62 @@ CREATE TABLE `students` (
   `pcNum` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `yearlevel` varchar(255) DEFAULT NULL,
+  `block` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `subject`, `yearlevel`, `block`) VALUES
+(1, 'Computer Programming 2 ', '1st Year', 'A'),
+(2, 'Web Development', '2nd Year', 'A'),
+(3, 'dsaddsad', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `profaccounts`
+--
+ALTER TABLE `profaccounts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `professor`
@@ -96,26 +202,62 @@ ALTER TABLE `students`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `profaccounts`
+--
+ALTER TABLE `profaccounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `professor`
 --
 ALTER TABLE `professor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `studaccounts`
 --
 ALTER TABLE `studaccounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
